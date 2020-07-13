aaf67d43f6fe9de9c3ec3383f4ef845235fa521c
Ed Cornejo <zed.hyde@icloud.com>
2020-07-12 23:06:15 -0500

    add support to docker pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -20,4 +20,18 @@ pipelineJob('theme-park-job') {
         }
     }
 }
-
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/EdCornejo/workshop-springboot.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
