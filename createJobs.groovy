ac096162f4b433335d6f09409c9067e84b44af9c
Ed Cornejo <zed.hyde@icloud.com>
2020-07-12 21:26:48 -0500

    update spring repository

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/EdCornejo/workshop-springbot.git'
                     }
                     branch 'master'
                 }
@@ -20,3 +20,4 @@ pipelineJob('theme-park-job') {
         }
     }
 }
+
